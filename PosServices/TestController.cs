804bf2a3cd5abd51686b20f3779188155e3ad0ba
Ivan Dutchak <ivan.dutchak@coop.dk>
2016-05-30 17:18:04 +0200

    Simple controller for testing

---
+++ PosServices/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace PosServices
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("/ping")]
+        public void Ping()
+        {
+        }
+
+        [HttpGet]
+        [Route("/test1")]
+        public string Test1()
+        {
+            return "test 1 response";
+        }
+
+        [HttpPost]
+        [Route("/test2")]
+        public string Test2()
+        {
+            return "test 2 response";
+        }
+    }
+}
